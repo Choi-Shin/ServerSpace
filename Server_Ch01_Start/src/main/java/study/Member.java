6e7359db1dc3027fd173a39a2d5e7327271dc8a8
choi-shin <shincho1990@naver.com>
2021-12-06 09:44:11 +0900

    211206

---
+++ Server_Ch01_Start/src/main/java/study/Member.java
@@ -0,0 +1,44 @@
+package study;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Member
+ */
+@WebServlet("/study/02/Member")
+public class Member extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		response.setCharacterEncoding("UTF-8");
+		request.setCharacterEncoding("utf-8");
+		PrintWriter out = response.getWriter();
+		
+		out.println("<html><body>");
+		
+		Enumeration<String> param = request.getParameterNames();
+		while(param.hasMoreElements()) {
+			String name = param.nextElement();
+			out.println(name + " : " + request.getParameter(name) + "<br>");
+		}
+		out.println("</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
