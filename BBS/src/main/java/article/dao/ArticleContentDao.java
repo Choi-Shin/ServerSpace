78950dc8f2a01c9337872b098799932d44a10439
choi-shin <shincho1990@naver.com>
2021-12-29 12:26:56 +0900

    211229

--- BBS/src/main/java/article/dao/ArticleContentDao.java
+++ BBS/src/main/java/article/dao/ArticleContentDao.java
@@ -1,7 +1,64 @@
 package article.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import article.model.ArticleContent;
+import jdbc.JdbcUtil;
+
 public class ArticleContentDao {
-	public int insert () {
-		return 0;
+	public ArticleContent insert (Connection conn, ArticleContent content) throws SQLException {
+		String sql = "Insert into article_content values(?, ?)";
+		PreparedStatement pstmt = null;
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setLong(1, content.getNumber());
+			pstmt.setString(2, content.getContent());
+			int result = pstmt.executeUpdate();
+			if(result > 0) {
+				return content;
+			} else {
+				return null;
+			}
+		} finally {
+			JdbcUtil.close(pstmt);
+		}
+	}
+	
+	public ArticleContent selectById(Connection conn, int no) throws SQLException {
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "select * from article_content where article_no = ?";
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, no);
+			rs = pstmt.executeQuery();
+			ArticleContent articleContent = null;
+			if(rs.next()) {
+				articleContent = 
+						new ArticleContent(rs.getInt("ARTICLE_NO"), rs.getString("content"));
+			}
+			return articleContent;
+		} finally {
+			JdbcUtil.close(rs);
+			JdbcUtil.close(pstmt);
+		}
+	}
+	
+	public int update(Connection conn, int no, String content) throws SQLException {
+		String sql = "update article_content set content = ? where article_no = ?";
+		PreparedStatement pstmt = null;
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, content);
+			pstmt.setInt(2, no);
+			return pstmt.executeUpdate();
+		} finally {
+			JdbcUtil.close(pstmt);
+		}
 	}
 }
